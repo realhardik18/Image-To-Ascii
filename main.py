3d8111dd83d2873b1a1b4dfc77d72e766d6cb594
hardik singh <realhardik18@gmail.com>
2022-02-01 13:22:14 +0530

    got basic model working
    now time to ENHANCE

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
-import cv2
-
-Image = cv2.imread("images/test.jpg")
-Image_blac_and_white = cv2.cvtColor(Image, cv2.COLOR_BGR2GRAY)
-(thresh, blackAndWhiteImage) = cv2.threshold(
-    Image_blac_and_white, 127, 255, cv2.THRESH_BINARY)
-cv2.imshow("final", Image_blac_and_white)
-cv2.waitKey(0)
-cv2.destroyAllWindows
+from PIL import Image
+from matplotlib import image
+image_file = Image.open("images/test.jpg")
+width, height = image_file.size
+newsize = (int(width/2), int(height/2))
+new_image = image_file.resize(newsize)
+new_image = new_image.convert('1')
+new_image.save('result.png')
+im = Image.open("result.png", 'r')
+pixel_vals = list(im.getdata())
+ascii_vals = []
+count = 1
+for val in pixel_vals:
+    if count % newsize[0] == 0:
+        ascii_vals.append("\n")
+        count += 1
+    else:
+        if val == 255:
+            ascii_vals.append("~")
+            count += 1
+        else:
+            ascii_vals.append(".")
+            count += 1
+f = open("vals.txt", "a")
+f.write(''.join(ascii_vals))
+f.close()
